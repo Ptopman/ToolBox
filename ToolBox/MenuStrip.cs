b6468cc5dd24737a2d40fa2b67b0c90d2daa29cd
Jameson Sullivan <Sullivan-jt@hotmail.com>
2020-01-26 12:50:04 -0500

    1.1.2.4

--- ToolBox/MenuStrip.cs
+++ ToolBox/MenuStrip.cs
@@ -53,7 +53,8 @@ namespace ToolBox
         //Opens task manager via method
         void IconOpenTask_click(object sender, EventArgs e)
         {
-            TaskMan();
+            //Open Task Manager
+            System.Diagnostics.Process.Start("Taskmgr.exe");
         }
 
         //Sets the flag to 1 to skip e.Cancel in Form1_FormClosing 
@@ -65,13 +66,15 @@ namespace ToolBox
         //calles the restart method when pressed
         void IconRestart_click(object sender, EventArgs e)
         {
-            Restart();
+            RestartShutdown rs = new RestartShutdown();
+            rs.Restart();
         }
 
         //called the shutdown method when pressed
         void IconShutdown_click(object sender, EventArgs e)
         {
-            Shutdown();
+            RestartShutdown rs = new RestartShutdown();
+            rs.Shutdown();
         }
 
         //Open pannel for computer information
@@ -83,95 +86,6 @@ namespace ToolBox
             SystemInformaitonForm SysInfoForm = new SystemInformaitonForm(f3);
             //Start and show the form
             SysInfoForm.Show();
-        }
-
-        //-----Methods-----
-        //Start task manager
-        private void TaskMan()
-        {
-            System.Diagnostics.Process.Start("Taskmgr.exe");
-        }
-
-        //Restart and shutdown prompts
-        private void BTNRestart_Click(object sender, EventArgs e)
-        {
-            Restart();
-        }
-
-        //Restart method
-        private void Restart()
-        {
-            //Displayed a yes / no dialog windows asking if you want to restart
-            DialogResult dialogResult = MessageBox.Show("Are you sure you want to restart?", "Restart", MessageBoxButtons.YesNo);
-            //If dialog window yes is pressed
-            if (dialogResult == DialogResult.Yes)
-            {
-                //New process
-                System.Diagnostics.Process process = new System.Diagnostics.Process();
-                //Process start information
-                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo
-                {
-                    //Keep window hidded
-                    WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden,
-                    //Process is Command prompt
-                    FileName = "cmd.exe",
-                    //Arguments to be passed to the command prompt
-                    Arguments = "/C shutdown /r /t 10"
-                    //Argument needs to start with /C, command is "Shutdown", /r is restart, /t is count down followed by number of seconds
-                };
-                //Passing the start information to the process start
-                process.StartInfo = startInfo;
-                //Start the process, command will run
-                process.Start();
-                //Once compleated, dispose of process
-                process.Dispose();
-            }
-            //if dialog window no is pressed
-            else if (dialogResult == DialogResult.No)
-            {
-                //NOTHING, DO NOTHING!!!
-            }
-        }
-
-        //Shutdown method
-        private void Shutdown()
-        {
-            //Displayed a yes / no dialog windows asking if you want to shutdown
-            DialogResult dialogResult = MessageBox.Show("Are you sure you want to shutdown?", "Shutdown", MessageBoxButtons.YesNo);
-            //If dialog window yes is pressed
-            if (dialogResult == DialogResult.Yes)
-            {
-                //new process
-                System.Diagnostics.Process process = new System.Diagnostics.Process();
-                //Process start information
-                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo
-                {
-                    //Keep window hidded
-                    WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden,
-                    //Process is command prompt
-                    FileName = "cmd.exe",
-                    //arguments to be passed to the command prompt
-                    Arguments = "/C shutdown /s /t 10"
-                    //Argument needs to start with /C, command is "Shutdown", /s is shutdown, /t is count down followed by number of seconds
-                };
-                //Passing the start information to the process start
-                process.StartInfo = startInfo;
-                //Start the process, command will run
-                process.Start();
-                //Once compleated, dispose of process
-                process.Dispose();
-            }
-            //If dialog window no is pressed
-            else if (dialogResult == DialogResult.No)
-            {
-                //NOTHING, DO NOTHING!!!
-            }
-        }
-
-        //call shutdown method when pressed
-        private void BTNShutdown_Click(object sender, EventArgs e)
-        {
-            Shutdown();
         }
     }
 }
--- ToolBox/ToolBox.cs
+++ ToolBox/ToolBox.cs
@@ -258,71 +258,15 @@ namespace ToolBox
         //Restart method
         private void Restart()
         {
-            //Displayed a yes / no dialog windows asking if you want to restart
-            DialogResult dialogResult = MessageBox.Show("Are you sure you want to restart?", "Restart", MessageBoxButtons.YesNo);
-            //If dialog window yes is pressed
-            if (dialogResult == DialogResult.Yes)
-            {
-                //New process
-                System.Diagnostics.Process process = new System.Diagnostics.Process();
-                //Process start information
-                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo
-                {
-                    //Keep window hidded
-                    WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden,
-                    //Process is Command prompt
-                    FileName = "cmd.exe",
-                    //Arguments to be passed to the command prompt
-                    Arguments = "/C shutdown /r /t 10"
-                    //Argument needs to start with /C, command is "Shutdown", /r is restart, /t is count down followed by number of seconds
-                };
-                //Passing the start information to the process start
-                process.StartInfo = startInfo;
-                //Start the process, command will run
-                process.Start();
-                //Once compleated, dispose of process
-                process.Dispose();
-            }
-            //if dialog window no is pressed
-            else if (dialogResult == DialogResult.No)
-            {
-                //NOTHING, DO NOTHING!!!
-            }
+            RestartShutdown rs = new RestartShutdown();
+            rs.Restart();
         }
 
         //Shutdown method
         private void Shutdown()
         {
-            //Displayed a yes / no dialog windows asking if you want to shutdown
-            DialogResult dialogResult = MessageBox.Show("Are you sure you want to shutdown?", "Shutdown", MessageBoxButtons.YesNo);
-            //If dialog window yes is pressed
-            if (dialogResult == DialogResult.Yes)
-            {
-                //new process
-                System.Diagnostics.Process process = new System.Diagnostics.Process();
-                //Process start information
-                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo
-                {
-                    //Keep window hidded
-                    WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden,
-                    //Process is command prompt
-                    FileName = "cmd.exe",
-                    //arguments to be passed to the command prompt
-                    Arguments = "/C shutdown /s /t 10"
-                    //Argument needs to start with /C, command is "Shutdown", /s is shutdown, /t is count down followed by number of seconds
-                };
-                //Passing the start information to the process start
-                process.StartInfo = startInfo;
-                //Start the process, command will run
-                process.Start();
-                //Once compleated, dispose of process
-                process.Dispose();
-            }
-            //If dialog window no is pressed
-            else if (dialogResult == DialogResult.No)
-            {
-                //NOTHING, DO NOTHING!!!
-            }
+            RestartShutdown rs = new RestartShutdown();
+            rs.Shutdown();
         }
 
         //call shutdown method when pressed
